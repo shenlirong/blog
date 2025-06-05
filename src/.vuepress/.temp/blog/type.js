2e23a69b2d5377d9d99d17fe20790d3408b0364b
shenlirong <shenlirong@inspur.com>
2025-06-05 17:19:59 +0800

    修改配置文件

--- src/.vuepress/.temp/blog/type.js
+++ src/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[13,0,8,7,6,16,17,9,10,11,12,14,15,18,1,2,5,3,4]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[9,13,14,0]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[13,8,7,6,16,17,9,10,11,12,14,15,0]}}}");
+export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[13,0,8,7,6,14,15,9,10,11,12,16,17,18,1,2,5,3,4]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[9,13,16,0]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[13,8,7,6,14,15,9,10,11,12,16,17,0]}}}");
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
