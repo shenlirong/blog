9d05e75232085f81acca35da65fa7e9ec5ac0916
shenlirong <shenlirong@inspur.com>
2025-06-05 19:20:51 +0800

    修改配置

--- src/.vuepress/.temp/git/config.js
+++ src/.vuepress/.temp/git/config.js
@@ -1,4 +1,4 @@
-import { GitContributors } from "C:/Users/slr/Desktop/Vue3学习资料/Vue3_test/test3/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
+import { GitContributors } from "C:/Users/slr/Desktop/Vue3学习资料/Vue3_test/11/test/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
 
 export default {
   enhance: ({ app }) => {
