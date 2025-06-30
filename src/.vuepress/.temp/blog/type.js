69a21d65bf5115ba9daca615417249105d880f77
shenlirong <shenlirong@inspur.com>
2025-06-30 20:59:38 +0800

    提交代码

--- src/.vuepress/.temp/blog/type.js
+++ src/.vuepress/.temp/blog/type.js
@@ -1,13 +1,2 @@
-export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[1,23,2,3,4,5,6,7,8,9,0,10,11,12,13,24,14,15,21,22,16,17,20,18,19,25,26,27]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[16,10,3,11,4,12,2,8,13]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[16]}}}");
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept();
-  if (__VUE_HMR_RUNTIME__.updateBlogType)
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-}
-
-if (import.meta.hot)
-  import.meta.hot.accept(({ typesMap }) => {
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-  });
+export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[1,23,2,3,4,5,6,7,8,9,0,10,11,12,13,24,22,21,14,15,16,17,20,18,19,25,26,27]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[10,11,12,13,2,8,3,4,16]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[10,11,5,6,7,12,13,2,8,22,24,21,3,9,4,25,0,23,26,1,27,14,19,18,20,17,15,16]}}}");
 
